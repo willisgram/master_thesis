2b8e2a35226bf5c88512594e44afa2fb9e5c9f76
willisgram <36190986+willisgram@users.noreply.github.com>
2018-03-20 19:23:30 +0100

    te

--- Rcode/R/points_forecasting/forecasting/17/16_17_glide/predictors.R
+++ Rcode/R/points_forecasting/forecasting/17/16_17_glide/predictors.R
@@ -5,7 +5,8 @@
 ###########
 # "Dummy" model:
 ###########
-
+library(tidyverse)
+library(xlsx)
 
 model_k = lm(realized ~ prev_4 + prev_3 + prev_2 + prev_1 + cost + opponent,data = regressors_16)
 
@@ -32,88 +33,90 @@ for (i in 1:10) {
   
   colnames(prev_points)[1:(k-1)] <- paste0("prev_",(k-1):1)
   
-}
 
-name <- paste0("round",i)
 
-#Cost
-  cost_round <- cost_round_17 %>% select(i+1)
-  colnames(cost_round)[1] <- "cost"
+  name <- paste0("round",i)
   
-
-#Pos
-  pos_round <- pos_round_17 %>% select(i+1)
-  colnames(pos_round)[1] <- "pos"
-  
-
-#trans_in
-  if(i == 1){
-    trans_in_round <- trans_in_round_16 %>% select(39)
-    colnames(trans_in_round)[1] <- "trans_in"
-  } else{
-    trans_in_round <- trans_in_round_17 %>% select(i+1)
-    colnames(trans_in_round)[1] <- "trans_in" 
-  }
-  
-#trans_out
-  if(i == 1){
-    trans_out_round <- trans_out_round_16 %>% select(39)
-    colnames(trans_out_round)[1] <- "trans_out"
-  } else{
-    trans_out_round <- trans_out_round_17 %>% select(i+1)
-    colnames(trans_out_round)[1] <- "trans_out" 
-  }
-
-## UPDATED IN FORECAST
-#Opponents
-  opponent_round <- opponent_round_17 %>% select(i+1)
-  colnames(opponent_round)[1] <- "opponent"
-  
-#Team
-  team_round <- team_round_17 %>% select(i+1)
-  colnames(team_round)[1] <- "team"
-  
-  for (j in 1:h) {
+  #Cost
+    cost_round <- cost_round_17 %>% select(i+1)
+    colnames(cost_round)[1] <- "cost"
     
-    if(j == 1){
-      #Create regressors
-      index <- data.frame(index = 1:625)
-      predictors <- cbind(index, prev_points,opponent_round,team_round,cost_round,
-                             pos_round,trans_in_round,trans_out_round)
-      #predictors$index <- as.factor(predictors$index)
-      
-      options(stringsAsFactors = T)
-      
-      ##Predict
-      predictions <- predict(object = model_k,newdata = predictors)
-      
-      
-      ##Store
-      name <- paste0("GW_",as.character(i))
-      predictions_table <- cbind(index,predictions)
-      colnames(predictions_table)[j+1] <- name
-      
-      
+  
+  #Pos
+    pos_round <- pos_round_17 %>% select(i+1)
+    colnames(pos_round)[1] <- "pos"
+    
+  
+  #trans_in
+    if(i == 1){
+      trans_in_round <- trans_in_round_16 %>% select(39)
+      colnames(trans_in_round)[1] <- "trans_in"
     } else{
-      
-      # u for update:
-      
-      opponent_u <- opponent_round_17 %>% select(i+j)
-      team_u     <- team_round_17 %>% select(i+j)
-      
-      # Create predictors
-      predictors[,"opponent"] <- opponent_u
-      predictors[,"team"] <- team_u
-      
-      ##New predictions
-      predictions_u <- predict(object = model_k,newdata = predictors)
-      
-      ##Store
-      name <- paste0("gw_",as.character(i+j-1))
-      predictions_table <- cbind(predictions_table,predictions_u)
-      colnames(predictions_table)[j+1] <- name
-      
+      trans_in_round <- trans_in_round_17 %>% select(i+1)
+      colnames(trans_in_round)[1] <- "trans_in" 
     }
+    
+  #trans_out
+    if(i == 1){
+      trans_out_round <- trans_out_round_16 %>% select(39)
+      colnames(trans_out_round)[1] <- "trans_out"
+    } else{
+      trans_out_round <- trans_out_round_17 %>% select(i+1)
+      colnames(trans_out_round)[1] <- "trans_out" 
+    }
+  
+  ## UPDATED IN FORECAST
+  #Opponents
+    opponent_round <- opponent_round_17 %>% select(i+1)
+    colnames(opponent_round)[1] <- "opponent"
+    
+  #Team
+    team_round <- team_round_17 %>% select(i+1)
+    colnames(team_round)[1] <- "team"
+    
+    for (j in 1:h) {
+      
+      if(j == 1){
+        #Create regressors
+        index <- data.frame(index = 1:625)
+        predictors <- cbind(index, prev_points,opponent_round,team_round,cost_round,
+                               pos_round,trans_in_round,trans_out_round)
+        #predictors$index <- as.factor(predictors$index)
+        
+        options(stringsAsFactors = T)
+        
+        ##Predict
+        predictions <- predict(object = model_k,newdata = predictors)
+        
+        
+        ##Store
+        name <- paste0("gw_",as.character(i))
+        predictions_table <- cbind(index,predictions)
+        colnames(predictions_table)[j+1] <- name
+        
+        
+      } else{
+        
+        # u for update:
+        
+        opponent_u <- opponent_round_17 %>% select(i+j)
+        team_u     <- team_round_17 %>% select(i+j)
+        
+        # Create predictors
+        predictors[,"opponent"] <- opponent_u
+        predictors[,"team"] <- team_u
+        
+        ##New predictions
+        predictions_u <- predict(object = model_k,newdata = predictors)
+        
+        ##Store
+        name <- paste0("gw_",as.character(i+j-1))
+        predictions_table <- cbind(predictions_table,predictions_u)
+        colnames(predictions_table)[j+1] <- name
+        
+      }
+    }
+    
     # Stucture back to standard format and write xlsx
     ###############
     
@@ -131,9 +134,8 @@ name <- paste0("round",i)
     rownames(predictions_table) <- NULL
     write.xlsx(predictions_table, file_for,row.names = F)
     #################
-    
+}  
   
-}
   
   
   
